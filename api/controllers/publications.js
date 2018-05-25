b65bc84c115c0e8ee0af0845718dab9bae8cf360
Alex Colombo <uricaine@hotmail.com>
2018-05-25 20:26:23 +0200

    creado metodo savePublication

--- api/controllers/publications.js
+++ api/controllers/publications.js
@@ -14,8 +14,29 @@ function probando(req, res) {
     return res.status(200).send({ message: "Hola Hola" })
 }
 
+function savePublication(req, res) {
+    let params = req.body;
 
+    if (!params.text) return res.status(200).send({ message: "Debes enviar un texto" })
+
+    let publication = new Publication();
+    publication.text = params.text;
+    publication.file = "null";
+    publication.user = req.user.sub;
+    publication.create_at = moment().unix()
+
+    publication.save((err, publicationStored) => {
+        if (err) return res.status(500).send({ message: "Error al guardar publication" })
+        if (!publicationStored) return res.status(404).send({ message: "La publicacion no ha sido guardada" })
+        return res.status(200).send({ publication: publicationStored })
+    })
+}
+
+function getPublications(req, res) {
+
+}
 
 module.exports = {
-    probando
+    probando,
+    savePublication
 }
--- api/routes/publications.js
+++ api/routes/publications.js
@@ -10,5 +10,6 @@ const multipart = require('connect-multiparty')
 const md_upload = multipart({ uploadDir: './uploads/publications' })
 
 api.get('/probando', md_auth.ensureAuth, PublicationController.probando)
+api.post('/publicar', md_auth.ensureAuth, PublicationController.savePublication)
 
 module.exports = api;
