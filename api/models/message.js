6679d60f12b86ed6cf0db28a1fe29465a9808477
Alex Colombo <uricaine@hotmail.com>
2018-05-26 10:26:59 +0200

    modificado modelo de datos mensaje

--- api/models/message.js
+++ api/models/message.js
@@ -5,6 +5,7 @@ const Schema = mongoose.Schema;
 
 const MessageSchema = Schema({
     text: String,
+    viewed: String,
     create_at: String,
     emiter: { type: Schema.ObjectId, ref: 'User' },
     receiver: { type: Schema.ObjectId, ref: 'User' }
