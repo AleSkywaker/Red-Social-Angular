6408a8f792432589692d9fb71ac66cfb6ee14be3
Alex Colombo <uricaine@hotmail.com>
2018-05-26 11:55:20 +0200

    añadido ruta para mensaje

--- api/app.js
+++ api/app.js
@@ -10,6 +10,7 @@ var app = express();
 const user_routes = require('../api/routes/user')
 const follow_routes = require('../api/routes/follow')
 const publication_routes = require('../api/routes/publications')
+const message_routes = require('../api/routes/message')
 
 //middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,6 +24,7 @@ app.use(bodyParser.json());
 app.use('/api', user_routes)
 app.use('/api', follow_routes)
 app.use('/api', publication_routes)
+app.use('/api', message_routes)
 
 
 //exportar configuracion
