f370622474e5d31d41836b0509bb0cec6248ab36
Alex Colombo <uricaine@hotmail.com>
2018-05-27 11:31:19 +0200

    añadido ruta para mensajes no leidos

--- api/routes/message.js
+++ api/routes/message.js
@@ -9,6 +9,7 @@ const md_auth = require('../middlewares/authentication')
 api.post('/guardar-message', md_auth.ensureAuth, MessageController.saveMessage)
 api.get('/trae-message/:page?', md_auth.ensureAuth, MessageController.getReceivedMessage)
 api.get('/messages-emitidos/:page?', md_auth.ensureAuth, MessageController.getEmitedMessage)
+api.get('/messages-noleidos', md_auth.ensureAuth, MessageController.getUnviewedMessages)
 
 
 module.exports = api;
