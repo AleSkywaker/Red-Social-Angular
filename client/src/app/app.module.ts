f746480c15b18492eca3a48bd8cc1cdd51a91332
Alex Colombo <uricaine@hotmail.com>
2018-05-28 19:59:27 +0200

    componentes creados

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,18 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "../components/login/login.component";
+import { RegisterComponent } from "../components/register/register.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, LoginComponent, RegisterComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
