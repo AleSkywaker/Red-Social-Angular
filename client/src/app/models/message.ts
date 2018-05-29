f7048bc08cb2899733385910d060695ade7bd107
Alex Colombo <uricaine@hotmail.com>
2018-05-29 20:36:17 +0200

    creado modelo message

---
+++ client/src/app/models/message.ts
@@ -0,0 +1,10 @@
+export class User {
+  constructor(
+    public _id: String,
+    public text: String,
+    public viewed: String,
+    public created_at: String,
+    public emiter: String,
+    public receiver: String
+  ) {}
+}
