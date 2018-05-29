7124fac78a8e43c85bfb289d9b18d9e9f3a7fd7a
Alex Colombo <uricaine@hotmail.com>
2018-05-29 20:36:40 +0200

    creado modelo user

---
+++ client/src/app/models/user.ts
@@ -0,0 +1,11 @@
+export class User {
+  constructor(
+    public _id: String,
+    public name: String,
+    public surname: String,
+    public nick: String,
+    public password: String,
+    public role: String,
+    public image: String
+  ) {}
+}
