7973c39a791dd24a8b91a020f36db126d63abe22
Alex Colombo <uricaine@hotmail.com>
2018-06-02 12:15:20 +0200

    añadido metodo get token

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -11,6 +11,10 @@ import { UserService } from "./../../services/user.service";
 export class LoginComponent implements OnInit {
   public titulo: string;
   public user: User;
+  public status: string;
+  public identity;
+  public token;
+  public name;
 
   constructor(
     private _router: Router,
@@ -18,13 +22,61 @@ export class LoginComponent implements OnInit {
     private _userService: UserService
   ) {
     this.titulo = "Identificate";
-    this.user = new User("", "", "", "", "", "ROLE_USER", "");
+    this.user = new User("", "", "", "", "", "ROLE_USER", "", "");
   }
 
   ngOnInit() {
     console.log("Componente de login cargado");
   }
   onSubmit() {
-    console.log(this.user);
+    //llamar al usuario y conseguir los datos
+    this._userService.signup(this.user).subscribe(
+      response => {
+        this.identity = response.user;
+        console.log(this.identity);
+        this.name = response.user.name;
+        if (!this.identity || !this.identity._id) {
+          this.status = "error";
+        } else {
+          this.status = "success";
+          //Persistir datos del usuario
+
+          //Conseguir tokken
+          this.getToken();
+        }
+      },
+      error => {
+        let errorMessage = <any>error;
+        console.log(errorMessage);
+        if (errorMessage != null) {
+          this.status = "error";
+        }
+      }
+    );
+    // console.log(this.user);subscribessubscribe
+  }
+
+  getToken() {
+    this._userService.signup(this.user, "true").subscribe(
+      response => {
+        this.token = response.token;
+        console.log(this.token);
+        if (this.token <= 0) {
+          this.status = "error";
+        } else {
+          this.status = "success";
+          //Persistir el toeken del usuario
+
+          //Conseguir contadores o estadisticas del usuario
+        }
+      },
+      error => {
+        let errorMessage = <any>error;
+        console.log(errorMessage);
+        if (errorMessage != null) {
+          this.status = "error";
+        }
+      }
+    );
   }
 }
