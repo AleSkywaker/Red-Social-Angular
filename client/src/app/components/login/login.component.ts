fd3bcf6b3923b976b36a7de71f4fcc4d926132c3
Alex Colombo <uricaine@hotmail.com>
2018-06-02 11:14:33 +0200

    desarrollo login controller

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from "@angular/core";
+import { Router, ActivatedRoute, Params } from "@angular/router";
+import { User } from "../../models/user";
+import { UserService } from "./../../services/user.service";
 
 @Component({
   selector: "login",
-  templateUrl: "./login.component.html"
+  templateUrl: "./login.component.html",
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
   public titulo: string;
+  public user: User;
 
-  constructor() {
+  constructor(
+    private _router: Router,
+    private _route: ActivatedRoute,
+    private _userService: UserService
+  ) {
     this.titulo = "Identificate";
+    this.user = new User("", "", "", "", "", "ROLE_USER", "");
   }
 
   ngOnInit() {
     console.log("Componente de login cargado");
+  }
+  onSubmit() {
+    console.log(this.user);
   }
 }
