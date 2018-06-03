2552778c33498be2562c1c7259d249f9e64d680a
Alex Colombo <uricaine@hotmail.com>
2018-06-03 10:17:36 +0200

    añadido metodo para login y logout enapp.component.ts

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from "@angular/core";
+import { UserService } from "./services/user.service";
+import { Router, ActivatedRoute, Params } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [UserService]
 })
-export class AppComponent {
-  title = 'app';
-  titulo = "Red Social"
+export class AppComponent implements OnInit, DoCheck {
+  public titulo = "Red Social";
+  public identity;
+
+  constructor(
+    private _userService: UserService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) {}
+
+  ngOnInit() {
+    this.identity = this._userService.getIdentity();
+    console.log("Desde App cpmponente", this.identity);
+  }
+
+  ngDoCheck() {
+    this.identity = this._userService.getIdentity();
+  }
+
+  logout() {
+    localStorage.clear();
+    this.identity = null;
+    this._router.navigate(['/'])
+  }
 }
