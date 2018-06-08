e394deff99c84cdba705b0e118398ad2c1724cee
Alex Colombo <uricaine@hotmail.com>
2018-06-08 22:56:46 +0200

    refactor app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, DoCheck } from "@angular/core";
 import { UserService } from "./services/user.service";
 import { Router, ActivatedRoute, Params } from "@angular/router";
+import { GLOBAL } from './services/global'
 
 @Component({
   selector: "app-root",
@@ -9,14 +10,18 @@ import { Router, ActivatedRoute, Params } from "@angular/router";
   providers: [UserService]
 })
 export class AppComponent implements OnInit, DoCheck {
-  public titulo = "Red Social";
+  public titulo: String;
   public identity;
+  public url: String;
 
   constructor(
     private _userService: UserService,
     private _route: ActivatedRoute,
     private _router: Router
-  ) {}
+  ) {
+    this.titulo = "Red Social";
+    this.url = GLOBAL.url;
+  }
 
   ngOnInit() {
     this.identity = this._userService.getIdentity();
