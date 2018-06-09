86a91c3f139fe62eac53ba0d4989ae1cfda49afa
Alex Colombo <uricaine@hotmail.com>
2018-06-09 10:36:28 +0200

    refactor en upload.service

--- client/src/app/services/upload.service.ts
+++ client/src/app/services/upload.service.ts
@@ -14,22 +14,24 @@ export class UploadService {
       var formData: any = new FormData();
       var xhr = new XMLHttpRequest();
 
-      for (var i = 0; i < files.length; i++) {
-        formData.append(name, files[i], files[i].name)
-      }
+      if (files) {
+        for (var i = 0; i < files.length; i++) {
+          formData.append(name, files[i], files[i].name)
+        }
 
-      xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-          if (xhr.status == 200) {
-            resolve(JSON.parse(xhr.response))
-          } else {
-            reject(xhr.response)
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState == 4) {
+            if (xhr.status == 200) {
+              resolve(JSON.parse(xhr.response))
+            } else {
+              reject(xhr.response)
+            }
           }
         }
+        xhr.open('POST', url, true);
+        xhr.setRequestHeader('Authorization', token);
+        xhr.send(formData)
       }
-      xhr.open('POST', url, true);
-      xhr.setRequestHeader('Authorization', token);
-      xhr.send(formData)
     })
   }
 }
