eca5e9b31f8c3ddc0fc65c9a736d58b66ef8e7b6
Alex Colombo <uricaine@hotmail.com>
2018-06-14 20:12:50 +0200

    añadido metodo en publication service

---
+++ client/src/app/services/publication.service.ts
@@ -0,0 +1,27 @@
+import { Observable } from "rxjs/Observable";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { GLOBAL } from "./global";
+import { Publication } from "../models/publication";
+
+@Injectable()
+export class PublicationService {
+  public stats: any;
+  public url;
+  public identity;
+  public token;
+
+  constructor(public _http: HttpClient) {
+    this.url = GLOBAL.url;
+  }
+
+  addPublication(token, publication): Observable<any> {
+    let params = JSON.stringify(publication);
+    let headers = new HttpHeaders().set('Content-Type', 'application/json')
+      .set('authorization', token)
+
+    return this._http.post(this.url + '/publicar', params, { headers: headers })
+  }
+
+
+}
