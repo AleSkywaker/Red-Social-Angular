caefd5016ae0a3e9e262bf4987fee354b7c32568
Alex Colombo <uricaine@hotmail.com>
2018-06-15 23:03:20 +0200

    llamando al servicio getPublications desde template

--- client/src/app/components/timeline/timeline.component.ts
+++ client/src/app/components/timeline/timeline.component.ts
@@ -3,11 +3,12 @@ import { ActivatedRoute, Params, Router } from "@angular/router";
 import { Publication } from './../../models/publication';
 import { GLOBAL } from '../../services/global';
 import { UserService } from './../../services/user.service';
+import { PublicationService } from './../../services/publication.service';
 
 @Component({
   selector: "timeline",
   templateUrl: "./timeline.component.html",
-  providers: [UserService]
+  providers: [UserService, PublicationService]
 })
 
 export class TimelineComponent implements OnInit {
@@ -16,11 +17,14 @@ export class TimelineComponent implements OnInit {
   public token;
   public titulo: string;
   public url: string;
+  public status: string;
+  public page;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _userService: UserService
+    private _userService: UserService,
+    private _publicationService: PublicationService
   ) {
     this.titulo = 'Timeline'
     this.identity = this._userService.getIdentity();
@@ -29,8 +33,21 @@ export class TimelineComponent implements OnInit {
   }
 
   ngOnInit() {
-
+    this.getPublications(this.page);
   }
 
-
+  getPublications(page) {
+    this._publicationService.getPublications(this.token, page).subscribe(
+      response => {
+        console.log(response);
+        this.status = 'success';
+      }, error => {
+        var errorMessage = <any>error;
+        console.log(errorMessage);
+        if (errorMessage != null) {
+          this.status = 'error';
+        }
+      }
+    )
+  }
 }
