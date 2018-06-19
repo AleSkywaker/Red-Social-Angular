442022979bfadb8a192ffff60f21359016dcd2c3
Alex Colombo <uricaine@hotmail.com>
2018-06-19 20:07:15 +0200

    importados servicios en profile component

--- client/src/app/components/profile/profile.component.ts
+++ client/src/app/components/profile/profile.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Params, Router } from "@angular/router";
-import { Publication } from './../../models/publication';
+import { User } from '../../models/user'
+import { Follow } from '../../models/follow'
 import { GLOBAL } from '../../services/global';
 import { UserService } from './../../services/user.service';
-import { PublicationService } from './../../services/publication.service';
+import { FollowService } from './../../services/follow.service';
+
 
 @Component({
   selector: "profile",
   templateUrl: "./profile.component.html",
-  providers: [UserService, PublicationService]
+  providers: [UserService, FollowService]
 })
 
 export class ProfileComponent implements OnInit {
   public titulo: String;
+  public url: String;
   constructor() {
-    this.titulo = "Profile"
+    this.titulo = "Profile";
+    this.url = GLOBAL.url;
   }
   ngOnInit() { }
 }
