71ea2f0632576e584f6ae4a1dc020124122a122d
Alex Colombo <uricaine@hotmail.com>
2018-06-24 09:00:09 +0200

    cambio html por body linea 62 (timeline.component)

--- client/src/app/components/timeline/timeline.component.ts
+++ client/src/app/components/timeline/timeline.component.ts
@@ -59,7 +59,7 @@ export class TimelineComponent implements OnInit {
             let arrayB = response.publications;
             this.publications = arrayA.concat(arrayB);
 
-            $("html, body").animate({ scrollTop: $('body').prop('scrollHeight') }, 500)
+            $("html, body").animate({ scrollTop: $('html').prop('scrollHeight') }, 500)
           }
 
           if (page > this.pages) {
