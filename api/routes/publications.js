d0897bad8864be2065bd31781e8c23983163b143
Alex Colombo <uricaine@hotmail.com>
2018-06-24 08:52:12 +0200

    creado ruta para publicacion usuario

--- api/routes/publications.js
+++ api/routes/publications.js
@@ -7,11 +7,14 @@ const md_auth = require('../middlewares/authentication')
 
 
 const multipart = require('connect-multiparty')
-const md_upload = multipart({ uploadDir: './uploads/publications' })
+const md_upload = multipart({
+    uploadDir: './uploads/publications'
+})
 
 api.post('/publicar', md_auth.ensureAuth, PublicationController.savePublication)
-api.get('/publications/:page?', md_auth.ensureAuth, PublicationController.getPublications)
 api.get('/publication/:id', md_auth.ensureAuth, PublicationController.getPublication)
+api.get('/publications/:page?', md_auth.ensureAuth, PublicationController.getPublications)
+api.get('/publications-user/:user/:page?', md_auth.ensureAuth, PublicationController.getPublicationsUser)
 api.delete('/publication/:id', md_auth.ensureAuth, PublicationController.deletePublication)
 api.post('/upload-image-pub/:id', [md_auth.ensureAuth, md_upload], PublicationController.uploadImage)
 api.get('/get-image-pub/:imageFile', PublicationController.getImages)
