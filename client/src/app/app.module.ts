6ebf9773dafbeabc206e2782eab9f08176b0a7e5
Alex Colombo <uricaine@hotmail.com>
2018-06-27 19:47:22 +0200

    añadidos componente following  en app.module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { TimelineComponent } from './components/timeline/timeline.component';
 import { MomentModule } from 'angular2-moment';
 import { PublicationsComponent } from "./components/publications/publications.component";
 import { ProfileComponent } from './components/profile/profile.component';
+import { FollowingComponent } from './components/following/following.component'
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { ProfileComponent } from './components/profile/profile.component';
     SidebarComponent,
     TimelineComponent,
     PublicationsComponent,
-    ProfileComponent
+    ProfileComponent,
+    FollowingComponent
   ],
   imports: [BrowserModule, FormsModule, routing, HttpClientModule, MomentModule],
   providers: [appRoutingProviders],
