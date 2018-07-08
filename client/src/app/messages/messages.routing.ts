9799139e202d610b24ab056c80e23c0b49df6e94
Alex Colombo <uricaine@hotmail.com>
2018-07-08 09:27:01 +0200

    creado messages routing

---
+++ client/src/app/messages/messages.routing.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//Componentes
+import { MainComponent } from './components/main/main.component';
+import { AddComponent } from './components/add/add.component';
+import { ReceiverComponent } from './components/receiver/receiver.component';
+import { SendedComponent } from './components/sended/sended.component';
+
+const messagesRoutes: Routes = [
+  {
+    path: 'mensajes',
+    component: MainComponent,
+    children: [
+      { path: '', redirectTo: 'recibidos', pathMatch: 'full' },
+      { path: 'enviar', component: AddComponent },
+      { path: 'recibidos', component: ReceiverComponent },
+      { path: 'enviados', component: SendedComponent }
+    ]
+  }
+]
+@NgModule({
+  imports: [
+    RouterModule.forChild(messagesRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class MessagesRoutingModule { }
