543f6b385b47afef2f953a581e0c4346eb03a0ed
Alex Colombo <uricaine@hotmail.com>
2018-07-08 09:26:36 +0200

    añadido declartations, imports, exports y providers message.modules

---
+++ client/src/app/messages/messages.module.ts
@@ -0,0 +1,37 @@
+// Modulos
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+//Rutas
+import { MessagesRoutingModule } from './messages.routing';
+
+//Componentes
+import { MainComponent } from './components/main/main.component';
+import { AddComponent } from './components/add/add.component';
+import { ReceiverComponent } from './components/receiver/receiver.component';
+import { SendedComponent } from './components/sended/sended.component';
+
+
+@NgModule({
+  declarations: [
+    MainComponent,
+    AddComponent,
+    ReceiverComponent,
+    SendedComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MessagesRoutingModule
+  ],
+  exports: [
+    MainComponent,
+    AddComponent,
+    ReceiverComponent,
+    SendedComponent
+  ],
+  providers: []
+})
+
+export class MessagesModule { }
