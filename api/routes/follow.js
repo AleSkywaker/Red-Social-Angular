3ab0fc64277d0dede653151d75e6bc92fb6b9ddf
Alex Colombo <uricaine@hotmail.com>
2018-07-11 22:21:54 +0200

    modifico nombre de ruta para follows

--- api/routes/follow.js
+++ api/routes/follow.js
@@ -9,7 +9,7 @@ api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow)
 api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow)
 api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers)
 api.get('/followed/:id?/:page?', md_auth.ensureAuth, FollowController.getFollwedUsers)
-api.get('/get-follows/:followed?', md_auth.ensureAuth, FollowController.geFollows)
+api.get('/get-my-follows/:followed?', md_auth.ensureAuth, FollowController.geFollows)
 
 
 module.exports = api;
