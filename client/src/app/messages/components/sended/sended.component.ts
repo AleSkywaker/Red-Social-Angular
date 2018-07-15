f78f76b44a8a7a16e8870f5409c2243c2e906bd7
Alex Colombo <uricaine@hotmail.com>
2018-07-15 13:15:28 +0200

    creando sended.component

--- client/src/app/messages/components/sended/sended.component.ts
+++ client/src/app/messages/components/sended/sended.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Message } from '../../../models/message';
+import { MessageService } from '../../../services/message.service';
+import { Follow } from '../../../models/follow';
+import { FollowService } from './../../../services/follow.service';
+import { User } from '../../../models/user';
+import { UserService } from './../../../services/user.service';
+import { GLOBAL } from '../../../services/global';
 
 @Component({
   selector: 'sended',
@@ -7,11 +15,23 @@ import { Component, OnInit, DoCheck } from '@angular/core';
 
 export class SendedComponent implements OnInit {
   public titulo: String;
+  public identity;
+  public token;
+  public url: string;
+  public status: string;
+  public messages: Message[];
 
-  constructor() {
+  constructor(
+    private _followService: FollowService,
+    private _messageService: MessageService,
+    private _userService: UserService
+  ) {
     this.titulo = "Mensajes enviados";
   }
   ngOnInit() {
     console.log("Componente sended de mensajeria cargado");
+  }
+  getMessage() {
+
   }
 }
