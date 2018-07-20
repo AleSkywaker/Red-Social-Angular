a7b232c46101f3e916df9fba7aa142cb2ff86740
Alex Colombo <uricaine@hotmail.com>
2018-07-20 21:30:41 +0200

    fix app.routing

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -2,10 +2,10 @@ import { ModuleWithProviders } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 //Compnentes
-import { LoginComponent } from "../app/components/login/login.component";
-import { RegisterComponent } from "../app/components/register/register.component";
-import { HomeComponent } from "../app/components/home/home.component";
-import { UserEditComponent } from "../app/components/user-edit/user-edit.component";
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { HomeComponent } from "./components/home/home.component";
+import { UserEditComponent } from "./components/user-edit/user-edit.component";
 import { UserComponent } from './components/users/users.component';
 import { TimelineComponent } from './components/timeline/timeline.component';
 import { ProfileComponent } from './components/profile/profile.component';
