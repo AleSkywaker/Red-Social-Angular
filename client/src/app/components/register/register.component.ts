dc255c266c26a5372ae0d3217bfe4ff72c5e7236
Alex Colombo <uricaine@hotmail.com>
2018-07-24 18:47:29 +0200

    comentado metodo para debugeo

--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -20,7 +20,7 @@ export class RegisterComponent implements OnInit {
     private _userService: UserService
   ) {
     this.titulo = "Registrate";
-    this.user = new User("", "", "", "", "", "ROLE_USER", "");
+    this.user = new User("", "", "", "", "", "", "ROLE_USER", "");
   }
 
   ngOnInit() {
@@ -28,19 +28,20 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(form) {
-    this._userService.registro(this.user).subscribe(
-      response => {
-        if (response.user && response.user._id) {
-          this.status = "success";
-          // console.log(response.user);
-          form.reset();
-        } else {
-          this.status = "error";
-        }
-      },
-      error => {
-        console.log(<any>error);
-      }
-    );
+    console.log("this user", this.user)
+    // this._userService.registro(this.user).subscribe(
+    //   response => {
+    //     if (response.user && response.user._id) {
+    //       this.status = "success";
+    //       // console.log(response.user);
+    //       form.reset();
+    //     } else {
+    //       this.status = "error";
+    //     }
+    //   },
+    //   error => {
+    //     console.log(<any>error);
+    //   }
+    // );
   }
 }
