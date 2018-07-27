bb42e322d28de0583aee5fbeacbac435ce619537
Alex Colombo <uricaine@hotmail.com>
2018-07-27 23:13:27 +0200

    creado sistema validacion contraseñas en user controller

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -24,59 +24,67 @@ function pruebas(req, res) {
 function saveUser(req, res) {
     let params = req.body;
     let user = new User();
-    if (params.name && params.surname && params.nick && params.email && params.password) {
-        user.name = params.name;
-        user.surname = params.surname;
-        user.nick = params.nick;
-        user.email = params.email;
-        user.role = 'ROLE_USER';
-        user.image = null;
 
-        // Controlar usuarios duplicados
-        User.find({
-            $or: [{
-                    email: user.email.toLowerCase()
-                },
-                {
-                    nick: user.nick.toLowerCase()
-                }
-            ]
-        }).exec((err, users) => {
-            if (err) return res.status(500).send({
-                message: 'Error en paticion de usuario'
-            })
+    if (params.password === params.password2) {
 
-            if (users && users.length >= 1) {
-                return res.status(200).send({
-                    message: 'Ya existe un usuario con este email o nick'
+        if (params.name && params.surname && params.nick && params.email && params.password) {
+            user.name = params.name;
+            user.surname = params.surname;
+            user.nick = params.nick;
+            user.email = params.email;
+            user.role = 'ROLE_USER';
+            user.image = null;
+
+            // Controlar usuarios duplicados
+            User.find({
+                $or: [{
+                        email: user.email.toLowerCase()
+                    },
+                    {
+                        nick: user.nick.toLowerCase()
+                    }
+                ]
+            }).exec((err, users) => {
+                if (err) return res.status(500).send({
+                    message: 'Error en paticion de usuario'
                 })
-            } else {
-                // Cifra la contraseña y guarda los datos
-                bcrypt.hash(params.password, null, null, (err, hash) => {
-                    user.password = hash;
 
-                    user.save((err, userStored) => {
-                        if (err) return res.status(500).send({
-                            message: 'Error al guardar el usuario'
-                        });
-
-                        if (userStored) {
-                            userStored.password = ";)";
-                            res.status(200).send({
-                                user: userStored
-                            })
-                        } else {
-                            res.status(404).send({
-                                message: 'So se ha registrado el usuario'
-                            })
-                        }
+                if (users && users.length >= 1) {
+                    return res.status(200).send({
+                        message: 'Ya existe un usuario con este email o nick'
                     })
-                })
-            }
-        })
+                } else {
+                    // Cifra la contraseña y guarda los datos
+                    bcrypt.hash(params.password, null, null, (err, hash) => {
+                        user.password = hash;
+
+                        user.save((err, userStored) => {
+                            if (err) return res.status(500).send({
+                                message: 'Error al guardar el usuario'
+                            });
+
+                            if (userStored) {
+                                userStored.password = ";)";
+                                res.status(200).send({
+                                    user: userStored
+                                })
+                            } else {
+                                res.status(404).send({
+                                    message: 'So se ha registrado el usuario'
+                                })
+                            }
+                        })
+                    })
+                }
+            })
+        } else {
+            res.status(200).send({
+                message: 'Debes rellenar todo los campos'
+            })
+        }
     } else {
         res.status(200).send({
-            message: 'Debes rellenar todo los campos'
+            message: 'Las contraseñas no son iguales'
         })
     }
 }
